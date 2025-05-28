73f980a7d6b1491eaae837e95b111c3531df9d68
yegor <yborysenko@stud.khpi.ucode-connect.study>
2025-05-29 02:17:57 +0300

    game.js fix'

--- game/Game.js
+++ game/Game.js
@@ -1,161 +1,185 @@
 import db from "../models/index.js";
-
 const Card = db.sequelize.models.card;
 
 class Game {
-    constructor(p1, p2) {
-        this.players = [p1, p2];
+    constructor(playerOne, playerTwo) {
+        this.players = [playerOne, playerTwo];
 
-        this.#setPlayersEvents();
-        this.#startGame();
+        this.#setupEventHandlers();
+        this.#initializeGame();
     }
-    
-    #setPlayersEvents() {
-        this.players.forEach((player, idx) => {
-            const opponent = this.players[(idx + 1) % 2];
 
+    /**
+     * Attach all necessary socket event listeners for both players.
+     */
+    #setupEventHandlers() {
+        this.players.forEach((player, index) => {
+            const opponent = this.players[(index + 1) % 2];
+
+            // Allow player to check if a card is currently active
             player.socket.on("checkCardIsActive", (cardId) => {
-                if (player.checkCardIsActive(cardId))
-                    player.socket.emit('cardIsActive', cardId);
-            });
-
-            player.socket.on('moveCardToBoard', (cardId) => {
-                if (player.moveCardToBoard(cardId)) {
-                    player.socket.emit('moveCardToBoard', cardId);
-                    opponent.socket.emit('oppMoveCardToBoard', player.board[player.board.length - 1]);
+                if (player.checkCardIsActive(cardId)) {
+                    player.socket.emit("cardIsActive", cardId);
                 }
             });
 
-            player.socket.on('attackCard', (attackerId, targetId) => {
-                let attacker = player.board.find(elem => elem.id === attackerId);
-                let target = opponent.board.find(elem => elem.id === targetId);
+            // Handle card being moved from hand to board
+            player.socket.on("moveCardToBoard", (cardId) => {
+                if (player.moveCardToBoard(cardId)) {
+                    player.socket.emit("moveCardToBoard", cardId);
+                    opponent.socket.emit("oppMoveCardToBoard", player.board.at(-1));
+                }
+            });
+
+            // Handle attacking an opponent's card
+            player.socket.on("attackCard", (attackerId, targetId) => {
+                const attacker = player.board.find(card => card.id === attackerId);
+                const target = opponent.board.find(card => card.id === targetId);
 
                 player.attackCardOnBoard(attackerId, target.attack_points);
                 opponent.attackCardOnBoard(targetId, attacker.attack_points);
                 attacker.isActive = false;
 
-                player.socket.emit('attackCard', attacker, target);
-                opponent.socket.emit('attackCard', target, attacker);
-
-                console.log('player.board');
-                console.log(player.board);
-                console.log('opponent.board');
-                console.log(opponent.board);
+                player.socket.emit("attackCard", attacker, target);
+                opponent.socket.emit("attackCard", target, attacker);
             });
 
-            player.socket.on('attackOpponent', (attackerId) => {
-                let attacker = player.board.find(elem => elem.id === attackerId);
+            // Handle attacking opponent directly
+            player.socket.on("attackOpponent", (attackerId) => {
+                const attacker = player.board.find(card => card.id === attackerId);
 
                 opponent.reduceHp(attacker.attack_points);
 
-                opponent.socket.emit('updatePlayerHp', opponent.hp);
-                player.oppSocket.emit('updateOppHp', opponent.hp);
+                opponent.socket.emit("updatePlayerHp", opponent.hp);
+                player.oppSocket.emit("updateOppHp", opponent.hp);
 
-                this.checkGameOver();
+                this.#checkGameOver();
             });
 
-            player.socket.on('selectCard', (cardId, isTarget, attackerId) => {
-                opponent.socket.emit('selectCard', cardId, isTarget, attackerId);
+            // Card selection (for attack/target logic)
+            player.socket.on("selectCard", (cardId, isTarget, attackerId) => {
+                opponent.socket.emit("selectCard", cardId, isTarget, attackerId);
             });
 
-            player.socket.on('unselectCard', (cardId) => {
-                opponent.socket.emit('unselectCard', cardId);
-            });
-            
-            player.socket.on('turnEnd', () => {
-                this.startTurn((idx + 1) % 2);
+            // Unselect previously selected card
+            player.socket.on("unselectCard", (cardId) => {
+                opponent.socket.emit("unselectCard", cardId);
             });
 
-            player.socket.on('playerDeckSize', () => {
-                let cardCount = player.cardDeck.length;
-                player.socket.emit('playerDeckSize', cardCount);
+            // End of turn logic
+            player.socket.on("turnEnd", () => {
+                this.startTurn((index + 1) % 2);
             });
 
-            player.socket.on('opponentDeckSize', () => {
-                let cardCount = opponent.cardDeck.length;
-                player.socket.emit('opponentDeckSize', cardCount);
+            // Report the size of the player's own deck
+            player.socket.on("playerDeckSize", () => {
+                player.socket.emit("playerDeckSize", player.cardDeck.length);
             });
 
-            player.socket.on('giveUp', () => {
-                player.socket.emit('gameOver', false);
-                opponent.socket.emit('gameOver', true);
-            })
+            // Report the size of the opponent's deck
+            player.socket.on("opponentDeckSize", () => {
+                player.socket.emit("opponentDeckSize", opponent.cardDeck.length);
+            });
+
+            // Handle surrender/give up
+            player.socket.on("giveUp", () => {
+                player.socket.emit("gameOver", false);
+                opponent.socket.emit("gameOver", true);
+            });
         });
     }
 
-    #startGame() {
-        let goesFirstIdx = Math.round(Math.random());
+    /**
+     * Start the initial state of the game, deal cards and notify players.
+     */
+    #initializeGame() {
+        const firstPlayerIndex = Math.round(Math.random());
+        this.#dealInitialCards(firstPlayerIndex);
 
-        this.#getStartCards(goesFirstIdx);
+        this.players.forEach((player, index) => {
+            const opponent = this.players[(index + 1) % 2];
 
-        this.players.forEach((player, idx) => {
-            const opponent = this.players[(idx + 1) % 2];
-
-            player.socket.emit('initPlayersData', JSON.stringify({
-                "player": {nickname: player.nickname, hand: player.hand, avatar: player.avatar, health: player.hp, mana: player.currMana},
-                "opponent": {nickname: opponent.nickname, handLength: opponent.hand.length, avatar: opponent.avatar, health: opponent.hp}
+            player.socket.emit("initPlayersData", JSON.stringify({
+                player: {
+                    nickname: player.nickname,
+                    hand: player.hand,
+                    avatar: player.avatar,
+                    health: player.hp,
+                    mana: player.currMana
+                },
+                opponent: {
+                    nickname: opponent.nickname,
+                    handLength: opponent.hand.length,
+                    avatar: opponent.avatar,
+                    health: opponent.hp
+                }
             }));
-            
         });
 
-        this.startTurn(goesFirstIdx);
+        this.startTurn(firstPlayerIndex);
     }
 
-    getCardsToHand(playerIdx, numOfCards) {
-        const player = this.players[playerIdx];
-        const opponent = this.players[(playerIdx + 1) % 2];
+    /**
+     * Deal starting cards to both players.
+     */
+    #dealInitialCards(firstPlayerIndex) {
+        const first = this.players[firstPlayerIndex];
+        const second = this.players[(firstPlayerIndex + 1) % 2];
 
-        for (let i = 0; i < numOfCards; i++) {
+        for (let i = 0; i < 3; i++) first.drawCard();
+        for (let i = 0; i < 4; i++) second.drawCard();
+    }
 
-            let card = player.drawCard();
+    /**
+     * Start a new turn for a player.
+     */
+    startTurn(playerIndex) {
+        const current = this.players[playerIndex];
+        const opponent = this.players[(playerIndex + 1) % 2];
 
+        current.setBoardCardsActive();
+        current.socket.emit("turn", current.board);
+        opponent.socket.emit("oppTurn");
+
+        this.drawCards(playerIndex, 1);
+        current.replenishMana();
+    }
+
+    /**
+     * Draw a number of cards for a player and inform both players.
+     */
+    drawCards(playerIndex, number) {
+        const player = this.players[playerIndex];
+        const opponent = this.players[(playerIndex + 1) % 2];
+
+        for (let i = 0; i < number; i++) {
+            const card = player.drawCard();
             if (card) {
-                player.socket.emit('updateCards', [card]);
-                opponent.socket.emit('updateOppCards', [card].length);
+                player.socket.emit("updateCards", [card]);
+                opponent.socket.emit("updateOppCards", 1);
             }
-            this.checkGameOver();
-        }
-    }
-
-    #getStartCards(goesFirstIdx) {
-        const goesFirst = this.players[goesFirstIdx];
-        const goesSecond = this.players[(goesFirstIdx + 1) % 2];
-        
-        for (let i = 0; i < 3; i++) {
-            goesFirst.drawCard();
         }
 
-        for (let i = 0; i < 4; i++) {
-            goesSecond.drawCard();
-        }
+        this.#checkGameOver();
     }
 
-    startTurn(playerIdx) {
-        const player = this.players[playerIdx];
-        const opponent = this.players[(playerIdx + 1) % 2];
-
-        player.setBoardCardsActive();
-
-        player.socket.emit('turn', player.board);
-        opponent.socket.emit('oppTurn');
-        
-        this.getCardsToHand(playerIdx, 1);
-        player.replenishMana();
-    }
-
-    checkGameOver() {
+    /**
+     * Check whether either player's health has dropped to zero.
+     */
+    #checkGameOver() {
         if (this.players[0].hp <= 0) {
-            this.gameOver(this.players[1], this.players[0]);
-        }
-
-        if (this.players[1].hp <= 0) {
-            this.gameOver(this.players[0], this.players[1]);
+            this.#endGame(this.players[1], this.players[0]);
+        } else if (this.players[1].hp <= 0) {
+            this.#endGame(this.players[0], this.players[1]);
         }
     }
 
-    gameOver(winner, loser) {
-        winner.socket.emit('gameOver', true);
-        loser.socket.emit('gameOver', false);
+    /**
+     * End the game and notify both players of the outcome.
+     */
+    #endGame(winner, loser) {
+        winner.socket.emit("gameOver", true);
+        loser.socket.emit("gameOver", false);
     }
 }
 
